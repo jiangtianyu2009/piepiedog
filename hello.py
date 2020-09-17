6a6cc1ba4e481a2e0b2340c5ac2a9f4cfc1a9f6d
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-09-17 14:17:10 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
+
 app = Flask(__name__)
 
 
@@ -10,3 +12,23 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
